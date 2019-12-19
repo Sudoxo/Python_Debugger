69104db14d63f3e2921024fa13cbbde966d72a8f
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2019-12-19 20:54:24 +0100

    Add arrays support

--- debugger.py
+++ debugger.py
@@ -3,11 +3,12 @@ import sys
 
 #YOUR FUNCTION
 def your_function(a,b):
-    c=3
-    a= a + 1
-    b = b + 2
-    a = a + b
-    c=5
+    sample_list = []
+    sample_list.append(a)
+    a += 7
+    sample_list.append(a)
+    sample_list.append(b)
+    sample_list[0] = b
 
 def trace_calls(frame, event, arg):
     
@@ -33,14 +34,43 @@ def trace_lines(frame, event, arg):
     variables = frame.f_code.co_varnames
     state = frame.f_locals
     line = frame.f_lineno
-
+    
     #Check for changes
     for v in variables:
         if v in state:
-            if state[v] != val[v]:
+            
+            #ARRAYS AND TUPLES
+            
+            if isinstance(val[v],(list,tuple)):
+                for it in range(0,len(state[v])):
+                    
+                    #If index already exist
+                    if(it<len(val[v])):
+                        #And It've changed
+                        if(val[v][it] != state[v][it]):
+                            print("Line " + str(line-1) + ": value in '" + v + "' on index number " + str(it) + " changed from " + str(val[v][it]) + " to " + str(state[v][it]))
+                            val[v][it] = state[v][it]
+                            
+                    #New element
+                    else:
+                        print("Line " + str(line-1) + ": " + str(state[v][it]) + " added to list '" + v + "' on index number " + str(it))
+                        val[v].append(state[v][it])
+                        
+            #SINGLE VARIABLES
+                        
+            elif state[v] != val[v]:
                 print("Line " + str(line-1) + ": '" + v + "' changed from " + str(val[v]) + " to " + str(state[v]))
-                val[v] = state[v]
+                #New array case
+                if(state[v] == []):
+                    val[v] = []
+                else:
+                    
+                    val[v] = state[v]
+            
+                    
+            
+                
     
 val = {}
 sys.settrace(trace_calls)
-your_function(1,4)
+your_function(1,10)
